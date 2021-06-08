708dd4c9fc367a85c8edc03bc77d357e1e4b3758
Yoner Arbey Silva Leal <yonerarbeysl@ufps.edu.co>
2021-06-08 15:00:23 -0500

    Mi primer commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+
+function mostrar() {
+    var archivo = document.getElementById("file").files[0];
+    var reader = new FileReader();
+    if (archivo!=null) {
+        document.getElementById('file').style = 'background-color: green;';
+        reader.readAsDataURL(archivo);
+        reader.onloadend = function () {
+            document.getElementById("img").src = reader.result;
+        }
+    }
+}
+
+function buttonChange(){
+    const img_path = document.getElementById('img').src;
+    const filename = img_path.replace(/^.*[\\\/]/, '');
+    if(filename != ''){
+        // Load the model.
+        mobilenet.load().then(model => {
+            // Classify the image.
+            model.classify(img).then(predictions => {
+                let array = predictions;
+                document.getElementById('col_1-1').innerHTML = array[0].className;
+                document.getElementById('col_1-2').innerHTML = array[1].className;
+                document.getElementById('col_1-3').innerHTML = array[2].className;
+
+                document.getElementById('col_2-1').innerHTML = parseFloat(array[0].probability).toFixed(4);
+                document.getElementById('col_2-2').innerHTML = parseFloat(array[0].probability).toFixed(4);
+                document.getElementById('col_2-3').innerHTML = parseFloat(array[0].probability).toFixed(4);
+            });
+        });
+    }else{
+        alert('¡Error!, inserte un archivo primero.');
+    }
+}
+
+
+
